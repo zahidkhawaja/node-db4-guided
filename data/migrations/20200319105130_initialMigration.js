799e4a0c2c710500e760d1cfa3d4118ad8d97e61
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-19 10:57:33 +0100

    wip

---
+++ data/migrations/20200319105130_initialMigration.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('species', tbl => {
+      tbl.increments() // id colum of unsigned ints, unique, incrementing, not null...
+      tbl.string('species_name', 128).notNullable()
+    })
+    .createTable('zoos', tbl => {
+      tbl.increments()
+      tbl.string('zoo_name', 128).notNullable()
+      tbl.string('address', 128).notNullable()
+    })
+    .createTable()
+    .createTable()
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists()
+    .dropTableIfExists()
+    .dropTableIfExists('zoos')
+    .dropTableIfExists('species')
+};
